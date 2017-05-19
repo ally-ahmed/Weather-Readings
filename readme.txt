8487e34d85dd8c3703d53f58c5d862636de860ed
mcnongmin <norbixin@live.com>
2017-05-19 22:22:48 +0300

    finished solution

---
+++ readme.txt
@@ -0,0 +1,16 @@
+## setup
+To run the script you will require pandas
+install pandas by running the following on your terminal
+
+pip install pandas
+
+find more information on installing pandas here
+http://pandas.pydata.org/pandas-docs/stable/install.html
+
+You can run the script by simpling running python weather.py
+
+##Assumptions
+There were some values that had an asterix "*". I ignored the asterix and perfomed the calculation.
+
+
+
---
+++ weather.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+file_path = 'weather.dat'
+
+# use sep=\s+ to use varying whitespace as the separator 
+data = pd.read_table(file_path, sep='\s+')
+
+df = pd.DataFrame(data)
+
+# remove the '*' in the dataframe in pandas
+df = df.replace('\*','',regex=True)
+
+# convert to numeric
+df = df.apply(pd.to_numeric, errors='coerce')
+
+# get the difference between 'Mxt' and 'Mnt'
+df['diff'] = df['MxT'] - df['MnT'] 
+
+# store the maximum spread as well as corresponding day
+max_diff = df['diff'].max()
+
+max_day = df.loc[df['diff'] == max_diff, 'Dy'].iloc[0]
+
+print max_day, max_diff
+
+
+
+
+
+
+
